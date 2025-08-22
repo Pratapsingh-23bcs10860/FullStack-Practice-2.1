75feee3d2a02dfafb691898be3e715e37aa94750
Pratap singh <pratapsi356@gmail.com>
2025-08-22 21:40:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function updateCount() {
+    const inputText = document.getElementById("inputText").value;
+    document.getElementById("charCount").textContent = inputText.length;
+}
+
+function updateCount2() {
+    const inputText2 = document.getElementById("inputText2").value;
+    document.getElementById("charCount2").textContent = inputText2.length;
+}
